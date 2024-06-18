158e47350ca9dc8a06e712c80b24232b323995a6
Curtis Miles <curtis@digitalbrilliance.ca>
2024-06-18 16:02:32 -0400

    fix: Login error due to wrong auth promise

--- auth.ts
+++ auth.ts
@@ -12,7 +12,7 @@ export const config = {
   theme: {
     logo: '/litn-logo.png',
   },
-  adapter: MongoDBAdapter(new Promise((resolve) => mongoDBClient)),
+  adapter: MongoDBAdapter(new Promise((resolve) => resolve(mongoDBClient))),
   providers: [
     Google({ clientId: process.env.GOOGLE_CLIENT_ID, clientSecret: process.env.GOOGLE_CLIENT_SECRET }),
     FacebookProvider({
@@ -43,6 +43,32 @@ export const config = {
       sendVerificationRequest,
       from: process.env.EMAIL_FROM,
     }),
+    // CredentialsProvider({
+    //   // The name to display on the sign in form (e.g. 'Sign in with...')
+    //   name: 'Credentials',
+    //   // The credentials is used to generate a suitable form on the sign in page.
+    //   // You can specify whatever fields you are expecting to be submitted.
+    //   // e.g. domain, username, password, 2FA token, etc.
+    //   credentials: {
+    //     username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+    //     password: { label: 'Password', type: 'password' },
+    //   },
+    //   authorize: async (credentials, req) => {
+    //     // Add logic here to look up the user from the credentials supplied
+    //     const user = { id: '1', name: 'J Smith', email: 'jsmith@example.com' };
+
+    //     if (user) {
+    //       // Any object returned will be saved in `user` property of the JWT
+    //       return user;
+    //     } else {
+    //       // If you return null or false then the credentials will be rejected
+    //       return null;
+    //       // You can also Reject this callback with an Error or with a URL:
+    //       // throw new Error('error message') // Redirect to error page
+    //       // throw '/path/to/redirect'        // Redirect to a URL
+    //     }
+    //   },
+    // }),
   ],
   basePath: '/api/auth',
   callbacks: {
